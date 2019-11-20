ee165eecdb6576ec126a66e178bedc46863cb8e0
wlm2012 <541253785@qq.com>
2019-11-20 15:56:22 +0800

    io

--- test/src/main/java/wlm/App.java
+++ test/src/main/java/wlm/App.java
@@ -8,6 +8,11 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        test();
+    }
+
+
+    public static void test() {
+
     }
 }
--- test/src/main/java/wlm/ioUtil.java
+++ test/src/main/java/wlm/ioUtil.java
@@ -1,7 +1,49 @@
 package wlm;
 
-public class ioUtil{
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+public class ioUtil {
     public static void main(String[] args) {
-        System.out.println("hello");
+        String path="C:/Users/lenovo2/Desktop/业务影像化.postman_collection.json";
+        readFile(path);
     }
+
+    public static void test() {
+        InputStream in = System.in;
+        InputStreamReader reader = new InputStreamReader(in);
+
+        BufferedReader bReader = new BufferedReader(reader);
+        try {
+            System.out.println(bReader.read());
+        } catch (IOException e) {
+
+            e.printStackTrace();
+        }
+    }
+
+    public static void readFile(String path) {
+        File file = new File(path);
+        FileReader fReader = null;
+        try {
+            fReader = new FileReader(file);
+            char[] buf=new char[1024];
+            int temp=0;
+            while((temp=fReader.read(buf))>0){
+                System.out.println(new String(buf,0,temp));
+            }
+        } catch (FileNotFoundException e) {
+
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
 }
