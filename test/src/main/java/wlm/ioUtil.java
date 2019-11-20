b238bb3349c883789a0dba7aef7131c768a128c5
wlm2012 <541253785@qq.com>
2019-11-20 17:03:39 +0800

    file

--- test/src/main/java/wlm/ioUtil.java
+++ test/src/main/java/wlm/ioUtil.java
@@ -2,16 +2,28 @@ package wlm;
 
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 
+import javax.print.DocFlavor.STRING;
+
 public class ioUtil {
     public static void main(String[] args) {
-        String path="C:/Users/lenovo2/Desktop/业务影像化.postman_collection.json";
-        readFile(path);
+        String FromPath = "C:\\Users\\lenovo2\\Desktop\\业务影像化.postman_collection.json";
+        String ToPath="";
+        
+        FromPath=RepalceSeparator(FromPath);
+        ToPath=RepalceSeparator(ToPath);
+        readFile(FromPath);
+    }
+
+    public static String RepalceSeparator(String s){
+        return s.replace("\\", File.separator).replace("/", File.separator);
     }
 
     public static void test() {
@@ -32,10 +44,10 @@ public class ioUtil {
         FileReader fReader = null;
         try {
             fReader = new FileReader(file);
-            char[] buf=new char[1024];
-            int temp=0;
-            while((temp=fReader.read(buf))>0){
-                System.out.println(new String(buf,0,temp));
+            char[] buf = new char[1024];
+            int temp = 0;
+            while ((temp = fReader.read(buf)) > 0) {
+                System.out.print(new String(buf, 0, temp));
             }
         } catch (FileNotFoundException e) {
 
@@ -44,6 +56,21 @@ public class ioUtil {
             e.printStackTrace();
         }
 
+    }
+
+    public static void MoveFile(String FromPath, String ToPath) {
+        File FromFile = new File(FromPath);
+        File ToFile = new File(ToPath);
+        FileInputStream fInputStream=null;
+        FileOutputStream fOutputStream=null;
+        try {
+            fInputStream = new FileInputStream(FromFile);
+            fOutputStream=new FileOutputStream(ToFile);
+
+        } catch (FileNotFoundException e) {
+
+            e.printStackTrace();
+        }
     }
 
 }
