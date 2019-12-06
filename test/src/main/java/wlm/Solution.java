89fde3c26ba91875c3c5fa2015e2b0c3e21e91b0
wlm2012 <541253785@qq.com>
2019-12-06 18:22:44 +0800

    solution

--- test/src/main/java/wlm/Solution.java
+++ test/src/main/java/wlm/Solution.java
@@ -2,19 +2,23 @@ package wlm;
 
 class Solution {
 
-    
-
-
-
-
-
-
+    public int balancedStringSplit(String s) {
+        int nums = 0;
+        int i = 0;
+        for (char c : s.toCharArray()) {
+            i = 'L' == c ? i + 1 : i - 1;
+            if (i == 0) {
+                nums++;
+            }
+        }
+        return nums;
+    }
 
     public int[] twoSum(int[] nums, int target) {
         for (int i = 0; i < nums.length; i++) {
             for (int j = i; j < nums.length; j++) {
-                if (target==(nums[i]+nums[j])) {
-                    int[] n={i,j};
+                if (target == (nums[i] + nums[j])) {
+                    int[] n = { i, j };
                     return n;
                 }
             }
@@ -22,47 +26,46 @@ class Solution {
         return null;
     }
 
-
-
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        int i=0;
-        int val1=0;
-        int val2=0;
+        int i = 0;
+        int val1 = 0;
+        int val2 = 0;
 
-        ListNode listNode=new ListNode(0);
-        ListNode cur=listNode;
-        while(null!=l1||null!=l2||1==i){
-            cur.next=new ListNode(0);
-            cur=cur.next;
-            val1=null==l1?0:l1.val;
-            val2=null==l2?0:l2.val;
-            if ((val1+val2+i)>9) {
-                cur.val=(val1+val2+i)%10;
-                i=1;
-            }else{
-                cur.val=val1+val2+i;
-                i=0;
+        ListNode listNode = new ListNode(0);
+        ListNode cur = listNode;
+        while (null != l1 || null != l2 || 1 == i) {
+            cur.next = new ListNode(0);
+            cur = cur.next;
+            val1 = null == l1 ? 0 : l1.val;
+            val2 = null == l2 ? 0 : l2.val;
+            if ((val1 + val2 + i) > 9) {
+                cur.val = (val1 + val2 + i) % 10;
+                i = 1;
+            } else {
+                cur.val = val1 + val2 + i;
+                i = 0;
             }
-            l1=l1==null?null:l1.next;
-            l2=l2==null?null:l2.next;
-
+            l1 = l1 == null ? null : l1.next;
+            l2 = l2 == null ? null : l2.next;
 
         }
 
         return listNode.next;
     }
 
-    public static  void main(String[] args) {
-        ListNode l1=new ListNode(5);
-/*         l1.next=new ListNode(4);
-        l1.next.next=new ListNode(3); */
+    public static void main(String[] args) {
+        /*
+         * ListNode l1=new ListNode(5); l1.next=new ListNode(4); l1.next.next=new
+         * ListNode(3);
+         * 
+         * ListNode l2=new ListNode(5); l2.next=new ListNode(6); l2.next.next=new
+         * ListNode(4); Solution solution=new Solution();
+         * 
+         * ListNode listNode=solution.addTwoNumbers(l1,l2);
+         * System.out.println(listNode.val+""+listNode.next.val+listNode.next.next.val);
+         */
 
-        ListNode l2=new ListNode(5);
-/*         l2.next=new ListNode(6);
-        l2.next.next=new ListNode(4); */
-        Solution solution=new Solution();
-        
-        ListNode listNode=solution.addTwoNumbers(l1,l2);
-        System.out.println(listNode.val+""+listNode.next.val+listNode.next.next.val);
+        Solution solution = new Solution();
+        solution.balancedStringSplit("RLRRLLRLRL");
     }
 }
--- test/src/main/java/wlm/TestException.java
+++ test/src/main/java/wlm/TestException.java
@@ -1,29 +1,87 @@
 package wlm;
 
+import java.io.IOException;
+
+public class TestException
+{
 
 
-
-public class TestException {
-
-
-    public static void testEx1() {
-        try {
-             int a=1/0;
-
-        } catch (Exception e) {
-            System.out.println("test");
-            // throw e;
-            // throw new IOException();
+    boolean testEx() throws Exception
+    {
+        boolean ret = true;
+        try
+        {
+            ret = testEx1();
+        }
+        catch (Exception e)
+        {
+            System.out.println("testEx, catch exception");
+            ret = false;
+            throw e;
+        }
+        finally
+        {
+            System.out.println("testEx, finally; return value=" + ret);
+            return ret;
         }
     }
-
-    public static void main(String[] args) {
-        
-        try {
-            testEx1();
-            System.out.println("1");
-        } catch (Exception e) {
-            System.out.println("main");
+    boolean testEx1() throws Exception
+    {
+        boolean ret = true;
+        try
+        {
+            ret = testEx2();
+            if (!ret)
+            {
+                return false;
+            }
+            System.out.println("testEx1, at the end of try");
+            return ret;
+        }
+        catch (Exception e)
+        {
+            System.out.println("testEx1, catch exception");
+            ret = false;
+            throw e;
+        }
+        finally{
+            System.out.println("testEx1, finally; return value=" + ret);
+            return ret;
+        }
+    }
+    boolean testEx2() throws IOException
+    {
+        boolean ret = true;
+        try
+        {
+            int b = 12;
+            int c;
+            for (int i = 2; i >= -2; i--)
+            {
+                c = b / i;
+                System.out.println("i=" + i);
+            }
+            return true;
+        }
+        catch (Exception e)
+        {
+            System.out.println("testEx2, catch exception");
+            ret = false;
+            throw new IOException();
+        }
+        finally{
+            System.out.println("testEx2, finally; return value=" + ret);
+            // ret=true;
+            // return ret;
+        }
+    }
+    public static void main(String[] args)
+    {
+        TestException testException1 = new TestException();
+        try{
+            testException1.testEx();
+        }
+        catch (Exception e){
             e.printStackTrace();
         }
     }
