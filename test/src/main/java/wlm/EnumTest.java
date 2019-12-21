325191fc5b7d8b5ab240b909888acc683de40ed6
wlm2012 <541253785@qq.com>
2019-12-21 10:31:56 +0800

    enum

--- test/src/main/java/wlm/EnumTest.java
+++ test/src/main/java/wlm/EnumTest.java
@@ -1,5 +1,31 @@
 package wlm;
 
+import java.util.Scanner;
+
 public class EnumTest {
-    
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("small, medium, large");
+        String input = in.next();
+        Size size = Enum.valueOf(Size.class, input);
+        System.out.println("size=" + size);
+        System.out.println("ab=" + size.getAd());
+        in.close();
+    }
+
+}
+
+enum Size {
+    small("s"), medium("m"), large("l");
+
+    private Size(String ad) {
+        this.ad = ad;
+    }
+
+    private String ad;
+
+    public String getAd() {
+        return ad;
+    }
 }
