4bdc1147ed48eb5dca024c00dcfecae2dfded269
wlm2012 <541253785@qq.com>
2020-01-02 17:39:31 +0800

    test

--- test/src/main/java/wlm/Test.java
+++ test/src/main/java/wlm/Test.java
@@ -3,18 +3,45 @@ package wlm;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Test {
 
     ArrayList<String> list = new ArrayList<>();
 
     public static void main(String[] args) {
+        test4();
+    }
+
+    public static void test4() {
+        int count = 123411;
+        int SlicePagNum = 1000;
+        int pageCount = count % SlicePagNum == 0 ? count / SlicePagNum : count / SlicePagNum + 1;
+        int[] acctNos = new int[pageCount * 2];
+        int pageNo = 0;
+        int i;
+        for (i = 0; i < acctNos.length; i += 2) {
+            acctNos[i] = 1 + pageNo * SlicePagNum;
+            acctNos[i + 1] = (pageNo + 1) * SlicePagNum;
+            pageNo++;
+        }
+        acctNos[acctNos.length - 1] = count;
+        String strIn = Arrays.toString(acctNos).substring(1);
+        strIn = strIn.substring(0, strIn.length() - 1);
+        System.out.println(strIn);
+    }
+
+    public static void test3() {
         String[] ss = { "11", "22" };
         for (String s : ss) {
             System.out.println(s);
         }
-        
+    }
 
+    public static void test2() {
+        int[] nums = { 1, 3, 4, 5 };
+        String string = Arrays.toString(nums);
+        System.out.println(string);
     }
 
     public static LocalDate parseLocalDate(String s) {
