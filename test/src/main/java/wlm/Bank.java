2247d32241e862385a22fcf85a8df636ce94900d
wlm2012 <541253785@qq.com>
2020-01-05 11:04:39 +0800

    do not know

--- test/src/main/java/wlm/Bank.java
+++ test/src/main/java/wlm/Bank.java
@@ -1,32 +1,49 @@
 package wlm;
 
 import java.util.Arrays;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class Bank {
     private final double[] accounts;
+    private Lock bankLock;
+    private Condition sufficientFunds;
 
     public Bank(int n, double initialBalance) {
         accounts = new double[n];
         Arrays.fill(accounts, initialBalance);
+        bankLock = new ReentrantLock();
+        sufficientFunds = bankLock.newCondition();
     }
 
     public void transfer(int from, int to, double amount) {
-        if (accounts[from] < amount) {
-            return;
+        bankLock.lock();
+        try {
+            if (accounts[from] < amount) {
+                return;
+            }
+            System.out.print(Thread.currentThread());
+            accounts[from] -= amount;
+            System.out.printf(" % 10.2f from %d to %d ", amount, from, to);
+            accounts[to] += amount;
+            System.out.printf(" Total Balance : %10.2f %n ", getTotalBalance());
+        } finally {
+            bankLock.unlock();
         }
-        System.out.print(Thread.currentThread());
-        accounts[from] -= amount;
-        System.out.printf(" % 10.2f from %d to %d ", amount, from, to);
-        accounts[to] += amount;
-        System.out.printf(" Total Balance : %10.2f %n ", getTotalBalance());
     }
 
     public double getTotalBalance() {
-        double sum = 0;
-        for (double a : accounts) {
-            sum += a;
+        bankLock.lock();
+        try {
+            double sum = 0;
+            for (double a : accounts) {
+                sum += a;
+            }
+            return sum;
+        } finally {
+            bankLock.unlock();
         }
-        return sum;
     }
 
     public int size() {
--- test/src/main/java/wlm/UnsynchBankTest.java
+++ test/src/main/java/wlm/UnsynchBankTest.java
@@ -2,7 +2,7 @@ package wlm;
 
 public class UnsynchBankTest {
     public static final int NACCOUNTS = 100;
-    public static final double INITIAL_BALANCE = 1000;
+    public static final double INITIAL_BALANCE = 10;
     public static final double MAX_AMOUNT = 1000;
     public static final int DELAY = 10;
 
