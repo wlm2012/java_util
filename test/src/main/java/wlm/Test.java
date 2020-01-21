5bb6b98dc734787c76cd3433d167bbdaf0672f49
wlm2012 <541253785@qq.com>
2020-01-21 11:39:38 +0800

    listtest

--- test/src/main/java/wlm/Test.java
+++ test/src/main/java/wlm/Test.java
@@ -9,69 +9,70 @@ import java.util.Arrays;
 
 public class Test {
 
-    ArrayList<String> list = new ArrayList<>();
+	ArrayList<String> list = new ArrayList<>();
 
-    public static void main(String[] args) {
-        test4();
-    }
+	public static void main(String[] args) {
+		Test test=new Test();
+		test.name();
+	}
 
-    public static void test4() {
-        int count = 123411;
-        int SlicePagNum = 1000;
-        int pageCount = count % SlicePagNum == 0 ? count / SlicePagNum : count / SlicePagNum + 1;
-        int[] acctNos = new int[pageCount * 2];
-        int pageNo = 0;
-        int i;
-        for (i = 0; i < acctNos.length; i += 2) {
-            acctNos[i] = 1 + pageNo * SlicePagNum;
-            acctNos[i + 1] = (pageNo + 1) * SlicePagNum;
-            pageNo++;
-        }
-        acctNos[acctNos.length - 1] = count;
-        String strIn = Arrays.toString(acctNos).substring(1);
-        strIn = strIn.substring(0, strIn.length() - 1);
-        System.out.println(strIn);
-    }
+	public static void test4() {
+		int count = 123411;
+		int SlicePagNum = 1000;
+		int pageCount = count % SlicePagNum == 0 ? count / SlicePagNum : count / SlicePagNum + 1;
+		int[] acctNos = new int[pageCount * 2];
+		int pageNo = 0;
+		int i;
+		for (i = 0; i < acctNos.length; i += 2) {
+			acctNos[i] = 1 + pageNo * SlicePagNum;
+			acctNos[i + 1] = (pageNo + 1) * SlicePagNum;
+			pageNo++;
+		}
+		acctNos[acctNos.length - 1] = count;
+		String strIn = Arrays.toString(acctNos).substring(1);
+		strIn = strIn.substring(0, strIn.length() - 1);
+		System.out.println(strIn);
+	}
 
-    public static void test3() {
-        String[] ss = { "11", "22" };
-        for (String s : ss) {
-            System.out.println(s);
-        }
-    }
+	public static void test3() {
+		String[] ss = {"11", "22"};
+		for (String s : ss) {
+			System.out.println(s);
+		}
+	}
 
-    public static void test2() {
-        int[] nums = { 1, 3, 4, 5 };
-        String string = Arrays.toString(nums);
-        System.out.println(string);
-    }
+	public static void test2() {
+		int[] nums = {1, 3, 4, 5};
+		String string = Arrays.toString(nums);
+		System.out.println(string);
+	}
 
-    public static LocalDate parseLocalDate(String s) {
-        try {
-            DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy-MM-dd");
-            LocalDate localDate = LocalDate.parse(s, dtf);
-            return localDate;
-        } catch (Exception e) {
-            throw e;
-        }
-    }
+	public static LocalDate parseLocalDate(String s) {
+		try {
+			DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+			LocalDate localDate = LocalDate.parse(s, dtf);
+			return localDate;
+		} catch (Exception e) {
+			throw e;
+		}
+	}
 
-    public void name() {
-        ListTest listTest = new ListTest();
+	public void name() {
+		ListTest listTest = new ListTest();
 
-        list.add("e");
-        listTest.list = list;
-        listTest.test2();
-        for (String s : list) {
-            System.out.println(s);
+		list.add("e");
+		listTest.setList(list);
+		listTest.test2();
+		for (String s : list) {
+			System.out.println(s);
 
-        }
-    }
+		}
+	}
 
-    public static void test1() {
-        Object d = null;
-        String s = (String) d;
-        System.out.println(s);
-    }
+	public static void test1() {
+		Object d = null;
+		String s = (String) d;
+		System.out.println(s);
+	}
 
 }
--- test/src/main/java/wlm/enumtest/EnumTest.java
+++ test/src/main/java/wlm/enumtest/EnumTest.java
@@ -4,21 +4,23 @@ import java.util.Scanner;
 
 public class EnumTest {
 
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        System.out.println("small, medium, large");
-        String input = in.next();
-        Size size = Enum.valueOf(Size.class, input);
-        System.out.println("string=" + Size.small.toString());
-        System.out.println("size=" + size);
-        System.out.println("ab=" + size.getAd());
-        in.close();
-    }
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		System.out.println("small, medium, large");
+		String input = in.next();
+		Size size = Enum.valueOf(Size.class, input);
+		System.out.println("string=" + Size.small.toString());
+		System.out.println("size=" + size);
+		System.out.println("ab=" + size.getAd());
+        System.out.println("s="+size.getS());
+		in.close();
+	}
 /*  input:   small
     output:
     string=small
     size=small
     ab=s
+    s=1
     
     */
 
@@ -26,15 +28,22 @@ public class EnumTest {
 }
 
 enum Size {
-    small("s"), medium("m"), large("l");
+	//ad  大小  s 数量
+	small("s", "1"), medium("m", "2"), large("l", "3");
 
-    private Size(String ad) {
-        this.ad = ad;
-    }
+	private Size(String ad, String s) {
+		this.ad = ad;
+		this.s = s;
+	}
 
-    private String ad;
+	private String ad;
+	private String s;
 
-    public String getAd() {
-        return ad;
-    }
+	public String getAd() {
+		return ad;
+	}
+
+	public String getS() {
+		return s;
+	}
 }
