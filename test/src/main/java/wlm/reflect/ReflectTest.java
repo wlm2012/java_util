4497258b1a8828fbc8fa49eb619e59d2596bdbb2
wlm2012 <541253785@qq.com>
2020-02-04 23:48:40 +0800

    test

--- test/src/main/java/wlm/reflect/ReflectTest.java
+++ test/src/main/java/wlm/reflect/ReflectTest.java
@@ -7,13 +7,17 @@ import wlm.enumtest.EnumTest;
  */
 public class ReflectTest {
 
+    public static void main(String[] args) {
+        test();
+    }
+
     public static void test() {
-        Class<EnumTest> class1=EnumTest.class;
-        String name=class1.getName();
-        System.out.println(name); 
+        Class<EnumTest> class1 = EnumTest.class;
+        String name = class1.getName();
+        System.out.println(name);
     }
 
     public static void test1() {
-        
+
     }
 }
